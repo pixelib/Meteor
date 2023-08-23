ef98424e08d564240d65cfbe12bc6de0d876695f
Duckelekuuk <duckelekuuk@gmail.com>
2023-08-23 18:18:38 +0200

    Fixed unittests

---
+++ rpcnis-core/src/main/java/com/npcnis/core/Penis.java
@@ -0,0 +1,8 @@
+package com.npcnis.core;
+
+public class Penis {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
---
+++ rpcnis-core/src/test/java/com/npcnis/cor/PenisTest.java
@@ -0,0 +1,14 @@
+package com.npcnis.cor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PenisTest {
+
+    @Test
+    public void testPenis() {
+        assertTrue(true);
+    }
+
+}
