d1d72bdafdc235d0a85320af3088e8d44ada67ed
Mats <mats@craftmend.com>
2023-08-23 17:55:54 +0200

    WIP tests

---
+++ rpcnis-core/src/test/java/tests/PenisTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class PenisTest extends TestCase {
+
+    @Test
+    public void testPenis() {
+        assertTrue(true);
+    }
+
+}
