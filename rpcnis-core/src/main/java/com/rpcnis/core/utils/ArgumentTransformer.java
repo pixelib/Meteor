577a66ecb8da3efe0ba842360e68ee6c86dae1dd
Mats <mats@craftmend.com>
2023-08-24 19:56:52 +0200

    Argument transformer + tests

---
+++ rpcnis-core/src/main/java/com/rpcnis/core/utils/ArgumentTransformer.java
@@ -0,0 +1,59 @@
+package com.rpcnis.core.utils;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgumentTransformer {
+
+    private static final Map<Class<?>, Class<?>> PRIMATIVE_TO_BOXED = Map.of(
+            boolean.class, Boolean.class,
+            byte.class, Byte.class,
+            char.class, Character.class,
+            double.class, Double.class,
+            float.class, Float.class,
+            int.class, Integer.class,
+            long.class, Long.class,
+            short.class, Short.class,
+            void.class, Void.class
+    );
+
+    /**
+     * Method arguments cannot be mapped one-to-one to an invocation, because the actual signatures
+     * may differ from the declared signatures (this can happen with optional arrays, for example).
+     * Luckily, arrays can only be declared as the last argument and there can only be one array,
+     * so we can use this to our advantage to simply overflown everything after l-1 into the array, and using that
+     * as the final argument.
+     */
+    public static Object[] overflowArguments(Method method, Object[] allArguments) {
+        Object[] output = new Object[method.getParameterCount()];
+
+        // map all arguments except the last one
+        if (method.getParameterCount() - 1 >= 0)
+            System.arraycopy(allArguments, 0, output, 0, method.getParameterCount() - 1);
+
+        // map the last argument as an array
+        Class<?> lastParameterType = method.getParameterTypes()[method.getParameterCount() - 1];
+        if (lastParameterType.isArray()) {
+            // create an array of the correct type
+            int length = allArguments.length - method.getParameterCount() + 1;
+            Class<?> componentType = PRIMATIVE_TO_BOXED.getOrDefault(lastParameterType.getComponentType(), lastParameterType.getComponentType());
+
+            Object[] array = (Object[]) java.lang.reflect.Array.newInstance(componentType, length);
+
+            // fill the array with the remaining arguments
+            if (allArguments.length - (method.getParameterCount() - 1) >= 0)
+                System.arraycopy(allArguments, method.getParameterCount() - 1, array, method.getParameterCount() - 1 - method.getParameterCount() + 1, allArguments.length - (method.getParameterCount() - 1));
+
+            // set the array as the last argument
+            output[output.length - 1] = array;
+        } else {
+            // set the last argument as-is
+            output[output.length - 1] = allArguments[allArguments.length - 1];
+        }
+
+        return output;
+    }
+
+
+}
---
+++ rpcnis-core/src/test/java/com/rpcnis/core/utils/ArgumentTransformerTest.java
@@ -0,0 +1,68 @@
+package com.rpcnis.core.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArgumentTransformerTest {
+
+    @Test
+    public void testExactMatch() {
+        Method method = getMethod("aMethodWithThreeArguments");
+        Object[] arguments = new Object[]{1, 2, 3};
+        Object[] transformedArguments = ArgumentTransformer.overflowArguments(method, arguments);
+        assertArrayEquals(arguments, transformedArguments);
+    }
+
+    @Test
+    public void testOverflow() {
+        Method method = getMethod("aMethodWithThreeArguments");
+        Object[] arguments = new Object[]{1, 2, 3};
+        Object[] transformedArguments = ArgumentTransformer.overflowArguments(method, arguments);
+        assertArrayEquals(new Object[]{1, 2, 3}, transformedArguments);
+    }
+
+    @Test
+    public void testOverflowWithArray() {
+        Method method = getMethod("aMethodWithOneArrayArgument");
+        Object[] arguments = new Object[]{1, 2, 3, 4, 5};
+        Object[] transformedArguments = ArgumentTransformer.overflowArguments(method, arguments);
+        assertArrayEquals(new Object[][]{{1, 2, 3, 4, 5}}, transformedArguments);
+    }
+
+    @Test
+    public void testOverflowWithOptionalArray() {
+        Method method = getMethod("aMethodWithAnOptionalArrayArgument");
+        Object[] arguments = new Object[]{1, 2, 3, 4, 5};
+        Object[] transformedArguments = ArgumentTransformer.overflowArguments(method, arguments);
+        assertArrayEquals(new Object[]{1, new Object[]{2, 3, 4, 5}}, transformedArguments);
+    }
+
+    private Method getMethod(String name) {
+        for (Method declaredMethod : getClass().getDeclaredMethods()) {
+            if (declaredMethod.getName().equals(name))
+                return declaredMethod;
+        }
+        fail("Method not found");
+        return null;
+    }
+
+    void aMethodWithThreeArguments(int a, int b, int c) {
+
+    }
+
+    void aMethodWithOneArgument(int a) {
+
+    }
+
+    void aMethodWithOneArrayArgument(int[] a) {
+
+    }
+
+    void aMethodWithAnOptionalArrayArgument(int a, int... b) {
+
+    }
+
+}
