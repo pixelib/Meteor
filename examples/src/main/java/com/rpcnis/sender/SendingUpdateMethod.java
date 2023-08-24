aca74f44e8fcd4d5459f4dba255c09b993f1c0e4
Duckelekuuk <duckelekuuk@gmail.com>
2023-08-24 17:47:50 +0200

    Add example for fancy wrapping

--- examples/src/main/java/com/rpcnis/sender/SendingUpdateMethod.java
+++ examples/src/main/java/com/rpcnis/sender/SendingUpdateMethod.java
@@ -3,11 +3,14 @@ package com.rpcnis.sender;
 import com.rpcnis.base.defaults.LoopbackTransport;
 import com.rpcnis.core.Rpcnis;
 
+import java.util.concurrent.CompletableFuture;
+
 public class SendingUpdateMethod {
 
     public static void main(String[] args) {
         Rpcnis rpcNis = new Rpcnis(new LoopbackTransport());
         MathFunctions mathFunctions = rpcNis.registerProcedures(MathFunctions.class);
+        MathFunctions mathFunctions2 = rpcNis.registerProcedures(MathFunctions.class, "Cooler-math-functions");
 
 
         int result = mathFunctions.add(1, 2, 3, 4, 5);
@@ -18,5 +21,6 @@ public class SendingUpdateMethod {
         int multiply(int x, int times);
         int add(int... numbers);
         int substract(int from, int... numbers);
+        CompletableFuture<Integer> calculateMeaningOfLife();
     }
 }
