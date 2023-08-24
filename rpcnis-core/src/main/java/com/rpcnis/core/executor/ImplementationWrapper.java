c669d1ce247c1a4c537f5cec5e2db271ebfeedf4
Mats <mats@craftmend.com>
2023-08-24 20:33:22 +0200

    Start on invocation wrapper and fix github tests

--- rpcnis-core/src/main/java/com/rpcnis/core/executor/ImplementationWrapper.java
+++ rpcnis-core/src/main/java/com/rpcnis/core/executor/ImplementationWrapper.java
@@ -1,10 +1,40 @@
 package com.rpcnis.core.executor;
 
+import com.rpcnis.base.errors.MethodInvocationException;
+import com.rpcnis.core.models.InvocationDescriptor;
+import com.rpcnis.core.utils.ArgumentTransformer;
+
+import java.lang.reflect.Method;
+
 public class ImplementationWrapper {
 
     private Object implementation;
     private String namespace;
 
+    public ImplementationWrapper(Object implementation, String namespace) {
+        this.implementation = implementation;
+        this.namespace = namespace;
+    }
 
+    public <T> T invokeOn(InvocationDescriptor invocationDescriptor, Class<T> returnType) throws NoSuchMethodException {
+        // Get the method that should be invoked
+        String methodName = invocationDescriptor.getMethodName();
+        Class<?>[] argTypes = invocationDescriptor.getArgTypes();
+
+        Method method = implementation.getClass().getMethod(methodName, argTypes);
+        // make accessible if private
+        method.setAccessible(true);
+
+        // sanitize args
+        Object[] args = ArgumentTransformer.overflowArguments(method, invocationDescriptor.getArgs());
+
+        // invoke method
+        try {
+            Object result = method.invoke(implementation, args);
+            return returnType.cast(result);
+        } catch (Exception e) {
+            throw new MethodInvocationException(invocationDescriptor.getMethodName(), namespace, e);
+        }
+    }
 
 }
