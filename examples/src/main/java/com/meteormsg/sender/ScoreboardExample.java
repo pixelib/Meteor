ab0e506f5950d64243da985089318a44c2c25b11
Mats <mats@craftmend.com>
2023-08-25 22:16:40 +0200

    Scoreboard example

---
+++ examples/src/main/java/com/meteormsg/sender/ScoreboardExample.java
@@ -0,0 +1,56 @@
+package com.meteormsg.sender;
+
+import com.meteormsg.base.defaults.LoopbackTransport;
+import com.meteormsg.core.Meteor;
+import com.meteormsg.transport.redis.RedisTransport;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ScoreboardExample {
+
+    public static void main(String[] args) throws Exception{
+        Meteor meteor = new Meteor(new LoopbackTransport());
+        meteor.registerImplementation(new ScoreboardImplementation(), "parkour-leaderboard");
+
+        Scoreboard scoreboard = meteor.registerProcedure(Scoreboard.class, "parkour-leaderboard");
+
+        scoreboard.setScoreForPlayer("player1", 10);
+        scoreboard.setScoreForPlayer("player2", 20);
+        scoreboard.setScoreForPlayer("player3", 30);
+
+        Map<String, Integer> scores = scoreboard.getAllScores();
+        System.out.println("scores: " + scores);
+
+        int player1Score = scoreboard.getScoreForPlayer("player1");
+        System.out.println("player1 score: " + player1Score);
+
+        meteor.stop();
+    }
+
+    public interface Scoreboard {
+        int getScoreForPlayer(String player);
+        void setScoreForPlayer(String player, int score);
+        Map<String, Integer> getAllScores();
+    }
+
+    public static class ScoreboardImplementation implements Scoreboard {
+
+        private final Map<String, Integer> scores = new HashMap<>();
+
+        @Override
+        public int getScoreForPlayer(String player) {
+            return scores.getOrDefault(player, 0);
+        }
+
+        @Override
+        public void setScoreForPlayer(String player, int score) {
+            scores.put(player, score);
+        }
+
+        @Override
+        public Map<String, Integer> getAllScores() {
+            return scores;
+        }
+    }
+}
