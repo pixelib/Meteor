706e01b9df1fe08e6d0d002e56eb94bb74130a9a
Duckelekuuk <duckelekuuk@gmail.com>
2023-08-29 21:33:25 +0200

    disable benchmarks and example being deployed

---
+++ meteor-jedis/src/test/java/dev/pixelib/meteor/transport/redis/RedisSubscriptionThreadTest.java
@@ -0,0 +1,20 @@
+package dev.pixelib.meteor.transport.redis;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RedisSubscriptionThreadTest {
+
+    @Test
+    void start_success() {
+    }
+
+    @Test
+    void subscribe_success() {
+    }
+
+    @Test
+    void stop_success() {
+    }
+}
